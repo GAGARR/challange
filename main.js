2c55cba18df40fe578db31c667ff5d179ae8d27f
Marcus M <78941703+GAGARR@users.noreply.github.com>
2021-11-16 09:18:37 +1000

    updated thats it
    lool

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('hi');
+console.log('Are you stuck?');
+
+
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
+
